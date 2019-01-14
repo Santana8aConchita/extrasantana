16aee77edc54ebcad5d218b2b52ec3d331395847
msantana0 <msantana0@ucol.mx>
2019-01-14 15:42:43 -0500

    #1 Correccion hecha

--- alta.php
+++ alta.php
@@ -19,10 +19,10 @@ if (isset($_GET["nom"])) {
         formRegistro();
     }else{
 
-                $query = 'INSERT INTO SALA_REMOTA (nombre, telefono, email_responsable, ip, issdn) VALUES ("'.$name.'","'.$responsable.'","'.$telefono.'","'.$email.'","'.$ip.'","'.$isdn.'")';
-                mysql_query($query) or die(mysql_error());
+                $query = 'INSERT INTO SALA_REMOTA (nombre, responsable, telefono, email_responsable, ip, isdn) VALUES ("'.$name.'","'.$responsable.'","'.$telefono.'","'.$email.'","'.$ip.'","'.$isdn.'")';
+                mysqli_query($conexion,$query) or die(mysqli_error($conexion));
                 echo 'La sala '.$name.' ha sido registrada de manera satisfactoria.<br/>';
-   header('Location: index.php');
+   header('Location: listar.php');
             }
         }
 
