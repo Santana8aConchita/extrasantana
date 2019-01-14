dccdf71bf240e35d249ec91d5ae133eba66a0e36
msantana0 <msantana0@ucol.mx>
2019-01-14 15:51:41 -0500

    #3 Se quitaron los errores y warnings

--- listar.php
+++ listar.php
@@ -17,8 +17,8 @@
     <!-- MetisMenu CSS -->
     <link href="css/plugins/metisMenu/metisMenu.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="css/sb-admin-2.css" rel="stylesheet">
+    <!-- Custom CSS 
+    <link href="css/sb-admin-2.css" rel="stylesheet">-->
  
 
 </head>
@@ -31,11 +31,11 @@ require "cfg/conexion.php";
 
    $query="select * from SALA_REMOTA";
 
-   $resultado=mysql_query($query);
+   $resultado=mysqli_query($conexion,$query);
    echo "<div class='container'>";
    echo "<table class='table table-striped'>";
    echo "<th> Nombre de Sala</th> <th>Respondable de Sala</th> <th>Telefono</th> <th>E-Mail</th> <th>IP</th> <th>ISDN</th> <th>Editar</th> <th>Eliminar</th>";
-   while ($dato=mysql_fetch_array($resultado)) {
+   while ($dato=mysqli_fetch_array($resultado)) {
    
    echo "<tr>";  
    echo "<td>". $dato['nombre']."</td>";
@@ -50,18 +50,18 @@ require "cfg/conexion.php";
    }
    echo "</table>";
 ?>
-   <button type="button" class="btn btn-success" onclick="window.location.href='sala_remota.html'">Angregar Sala</button>
+   <button type="button" class="btn btn-success" onclick="window.location.href='sala_remota.html'">Agregar Sala</button>
    </div>
     <!-- jQuery -->
-    <script src="../js/jquery.js"></script>
+    <link href="/js/jquery.js"/>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="../js/bootstrap.min.js"></script>
+    <link href="/js/bootstrap.min.js"/>
 
     <!-- Metis Menu Plugin JavaScript -->
-    <script src="../js/plugins/metisMenu/metisMenu.min.js"></script>
+    <link href="/js/plugins/metisMenu/metisMenu.min.js"/>
 
     <!-- Custom Theme JavaScript -->
-    <script src="../.js/sb-admin-2.js"></script>
+    <link href="/js/sb-admin-2.js"/>
 
 </body>
